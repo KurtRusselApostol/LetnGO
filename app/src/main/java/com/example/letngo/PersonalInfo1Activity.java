846b94e5d46ecbe94bac1ae4144f7b42d7d67988
RonnieMM29 <110813303+RonnieMM29@users.noreply.github.com>
2022-08-09 11:06:21 +0800

    deleted personal_info added PersonalInfo1Activity

---
+++ app/src/main/java/com/example/letngo/PersonalInfo1Activity.java
@@ -0,0 +1,14 @@
+package com.example.letngo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalInfo1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_info_1);
+    }
+}
