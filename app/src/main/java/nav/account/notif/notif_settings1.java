b0c9866e39d1a2e42634f28ed27fc2a824082c12
angelo-perez <92904983+angelo-perez@users.noreply.github.com>
2022-09-01 15:26:34 +0800

    - Changed the content of the Accounts > Notification section and removed some contents of it. - Notification selection/preference is now being stored in SharedPreferences

--- app/src/main/java/nav/account/notif/notif_settings1.java
+++ app/src/main/java/nav/account/notif/notif_settings1.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +18,14 @@ import com.example.letngo.R;
 
 public class notif_settings1 extends AppCompatActivity {
 
-    TextView save;
+    //TextView save;
     ImageButton back;
+    TableRow allNotif, accActivity, guessPolicies, offersUpdates, reminder;
+    Switch allNotifSwitch, accActivitySwitch, guessPoliciesSwitch, offersUpdatesSwitch, reminderSwitch;
+    //int numNotifOff = 0;
+    boolean accActivityOn, guessPoliciesOn, offersUpdatesOn, reminderOn;
+
     //
-    RadioGroup notifRadioGroup;
-    RadioButton getNotif, dontGetNotif;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     //
@@ -30,55 +36,188 @@ public class notif_settings1 extends AppCompatActivity {
         setContentView(R.layout.account_notif_settings1);
 
         sharedPreferences = getSharedPreferences("pref", 0); //file that saves notification preference
-        int notifSP = sharedPreferences.getInt("notifSP", 3); //sets radio button to unselected at first
+        int allNotifSP = sharedPreferences.getInt("allNotifSP", 0); //sets radio button to unselected at first
+        int accActivitySP = sharedPreferences.getInt("accActivitySP", 0); //sets radio button to unselected at first
+        int guessPoliciesSP = sharedPreferences.getInt("guessPoliciesSP", 0); //sets radio button to unselected at first
+        int offersUpdatesSP = sharedPreferences.getInt("offersUpdatesSP", 0); //sets radio button to unselected at first
+        int reminderSP = sharedPreferences.getInt("reminderSP", 0); //sets radio button to unselected at first
+
         editor = sharedPreferences.edit();
 
-        notifRadioGroup = findViewById(R.id.notifRadioGroup);
-        getNotif = findViewById(R.id.radioButton);
-        dontGetNotif = findViewById(R.id.radioButton2);
+        allNotif = findViewById(R.id.allNotif);
+        accActivity = findViewById(R.id.accActivity);
+        guessPolicies = findViewById(R.id.guessPolicies);
+        offersUpdates = findViewById(R.id.offersUpdates);
+        reminder = findViewById(R.id.reminder);
 
-        if (notifSP == 1){
-            getNotif.setChecked(true); //checks get notification radio button
-        }else if (notifSP == 0){
-            dontGetNotif.setChecked(true); //checks dont get notification radio button
-        }
+        allNotifSwitch = findViewById(R.id.allNotifSwitch);
+        accActivitySwitch = findViewById(R.id.accActivitySwitch);
+        guessPoliciesSwitch = findViewById(R.id.guessPoliciesSwitch);
+        offersUpdatesSwitch = findViewById(R.id.offersUpdatesSwitch);
+        reminderSwitch = findViewById(R.id.reminderSwitch);
 
-
-        save = findViewById(R.id.textView81);
         back = findViewById(R.id.img_back);
         back.setOnClickListener(v -> onBackPressed());
 
-        if(dontGetNotif.isChecked()){
-            save.setOnClickListener(v -> onBackPressed()); //sets the 'Save' button into a back button when 'dont get notification' section is set
-        }
-
-    }
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radioButton:
-                if (checked){
-                    save.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            editor.putInt("notifSP", 1);
-                            Intent intent = new Intent(notif_settings1.this, notif_settings2.class);
-                            startActivity(intent);
-                        }
-                    });
-                }
+//        notifRadioGroup = findViewById(R.id.notifRadioGroup);
+//        getNotif = findViewById(R.id.radioButton);
+//        dontGetNotif = findViewById(R.id.radioButton2);
+        switch (allNotifSP) {
+            case 0:
+                allNotifSwitch.setChecked(false);
+                accActivity.setVisibility(View.GONE);
+                guessPolicies.setVisibility(View.GONE);
+                offersUpdates.setVisibility(View.GONE);
+                reminder.setVisibility(View.GONE);
                 break;
-            case R.id.radioButton2:
-                if (checked) {
-                    editor.putInt("notifSP", 0);
-                    save.setOnClickListener(v -> onBackPressed());
-                }
+            case 1:
+                allNotifSwitch.setChecked(true);
                 break;
         }
-        editor.commit();
+        switch (accActivitySP) {
+            case 0:
+                accActivitySwitch.setChecked(false);
+                accActivityOn = false;
+                break;
+            case 1:
+                accActivitySwitch.setChecked(true);
+                accActivityOn = true;
+                break;
+        }
+        switch (guessPoliciesSP){
+            case 0:
+                guessPoliciesSwitch.setChecked(false);
+                guessPoliciesOn = false;
+                break;
+            case 1:
+                guessPoliciesSwitch.setChecked(true);
+                guessPoliciesOn = true;
+                break;
+        }
+        switch (offersUpdatesSP){
+            case 0:
+                offersUpdatesSwitch.setChecked(false);
+                offersUpdatesOn = false;
+                break;
+            case 1:
+                offersUpdatesSwitch.setChecked(true);
+                offersUpdatesOn = true;
+                break;
+        }
+        switch (reminderSP){
+            case 0:
+                reminderSwitch.setChecked(false);
+                reminderOn = false;
+                break;
+            case 1:
+                reminderSwitch.setChecked(true);
+                reminderOn = true;
+                break;
+        }
 
+        allNotifSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked == true){
+                    accActivity.setVisibility(View.VISIBLE);
+                    guessPolicies.setVisibility(View.VISIBLE);
+                    offersUpdates.setVisibility(View.VISIBLE);
+                    reminder.setVisibility(View.VISIBLE);
+                    accActivitySwitch.setChecked(true);
+                    guessPoliciesSwitch.setChecked(true);
+                    offersUpdatesSwitch.setChecked(true);
+                    reminderSwitch.setChecked(true);
+                    editor.putInt("allNotifSP", 1);
+                    editor.putInt("accActivitySP", 1);
+                    editor.putInt("guessPoliciesSP", 1);
+                    editor.putInt("offersUpdatesSP", 1);
+                    editor.putInt("reminderSwitchSP", 1);
+                }
+                else{
+                    accActivity.setVisibility(View.GONE);
+                    guessPolicies.setVisibility(View.GONE);
+                    offersUpdates.setVisibility(View.GONE);
+                    reminder.setVisibility(View.GONE);
+                    editor.putInt("allNotifSP", 0);
+                }
+                editor.commit();
+            }
+        });
+
+        accActivitySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked == true){
+                    accActivityOn = true;
+                    editor.putInt("accActivitySP", 1);
+                }
+                else{
+                    accActivityOn = false;
+                    SetAllNotifOff();
+                    editor.putInt("accActivitySP", 0);
+                }
+                editor.commit();
+            }
+        });
+
+        guessPoliciesSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked == true){
+                    guessPoliciesOn = true;
+                    editor.putInt("guessPoliciesSP", 1);
+                }
+                else{
+                    guessPoliciesOn = false;
+                    SetAllNotifOff();
+                    editor.putInt("guessPoliciesSP", 0);
+                }
+                editor.commit();
+            }
+        });
+
+        offersUpdatesSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked == true){
+                    offersUpdatesOn = true;
+                    editor.putInt("offersUpdatesSP", 1);
+                }
+                else{
+                    offersUpdatesOn = false;
+                    SetAllNotifOff();
+                    editor.putInt("offersUpdatesSP", 0);
+                }
+                editor.commit();
+            }
+        });
+
+        reminderSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked == true){
+                    reminderOn = true;
+                    editor.putInt("reminderSP", 1);
+                }
+                else{
+                    reminderOn = false;
+                    SetAllNotifOff();
+                    editor.putInt("reminderSP", 0);
+                }
+                editor.commit();
+            }
+        });
     }
+    protected void SetAllNotifOff(){
+        if (accActivityOn == false && guessPoliciesOn == false && offersUpdatesOn == false && reminderOn == false){
+            allNotifSwitch.setChecked(false);
+            accActivity.setVisibility(View.GONE);
+            guessPolicies.setVisibility(View.GONE);
+            offersUpdates.setVisibility(View.GONE);
+            reminder.setVisibility(View.GONE);
+            editor.putInt("allNotifSP", 0);
+            editor.commit();
+        }
+    }
+
 }
