3162af73ba94429630320a75152f53d33ef2df7a
Mike <vincentmike2001@gmail.com>
2022-09-05 22:21:44 +0800

    account_feedback.xml: modify texts and buttons

--- app/src/main/java/nav/account/Feedback.java
+++ app/src/main/java/nav/account/Feedback.java
@@ -1,21 +1,70 @@
 package nav.account;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.letngo.R;
 
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
 public class Feedback extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_feedback);
 
+        //backbutton
         ImageButton backButton = findViewById(R.id.back_btn);
-
         backButton.setOnClickListener(v -> onBackPressed());
+
+
+        //Spinners
+        Spinner spinner = findViewById(R.id.spinner_topic);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ppolicy_feedback, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setPrompt("Please select");
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String text = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Spinner spinner1 = findViewById(R.id.spinner_subtopic);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.ppolicy_feedback, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setPrompt("Please select");
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                String text = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
